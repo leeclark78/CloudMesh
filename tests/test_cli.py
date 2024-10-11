58b558e98c19dbcae5d3dcf212a4e18963c5a408
Lee Clark <lee.clark@tracsis.com>
2024-10-11 15:19:43 +0100

    remove files

--- tests/test_cli.py
+++
@@ -1,8 +0,0 @@
-from click.testing import CliRunner
-from cloudmesh.cli import hello
-
-def test_hello_command():
-    runner = CliRunner()
-    result = runner.invoke(hello)
-    assert result.exit_code == 0
-    assert "Hello from CloudMesh!" in result.output
